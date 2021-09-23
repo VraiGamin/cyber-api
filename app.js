855fd99c48c49efad99f6f050f8a1deddc00b24d
Leo <leo.gabaix@viacesi.fr>
2021-09-23 11:15:09 +0200

    added dotenv pkg

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const userRoutes = require('./routes/user.route');
-
-//connection to bdd
-//atlas: mongodb+srv://leo:passEdEmoT@cluster-one.9usqa.mongodb.net/db_wfo?retryWrites=true&w=majority
-//local : mongodb://localhost/wfo_local
-// mongoose.connect('mongodb+srv://user1:BTIkKDs3z6Jx8PyY@cluster0.m92sx.mongodb.net/db_eats?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-//   .then(() => console.log('Connexion à MongoDB réussie !'))
-//   .catch(() => console.log('Connexion à MongoDB échouée !'));
+const freezebeRoutes = require('./routes/freezebe.route');
 
 const app = express();
 
@@ -17,6 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //routing
-app.use('/users', userRoutes);
+app.use('/freezebe', freezebeRoutes);
 
 module.exports = app;
