fdbdd4d9e3a470c886a77c08dc92f1e2312d614d
Leo <leo.gabaix@viacesi.fr>
2021-09-23 11:14:44 +0200

    deleted examples files

--- controllers/user.ctrl.js
+++
@@ -1,9 +0,0 @@
-// const User = require('../models/User');
-// const ObjectId = require('mongodb').ObjectId;
-
-exports.getAllUsers = (req, res, next) => {
-    // User.find()
-    // .then(users => res.status(200).json(users))
-    // .catch(error => res.status(400).json({ error }))
-    console.log("HELLO");
-}
