2b8bda5912892dc7c8604437215da1992bce867c
Leo <leo.gabaix@viacesi.fr>
2021-09-27 13:37:47 +0200

    freezebes routes working + error catching

--- controllers/freezebe.ctrl.js
+++ controllers/freezebe.ctrl.js
@@ -15,64 +15,196 @@ var config = {
 		database: `${process.env.DB_NAME}`,
 	},
 };
-
 exports.getAll = (req, res, next) => {
-	// res.status(200).json({ "msg": "ok" })
+	let errorDetected = false;
 	var connection = new Connection(config);
 	connection.on("connect", function (err) {
-		// If no error, then good to proceed.
-		console.log("CONNEXION ON");
-		console.log("ERREUR ? ", err);
-		executeStatement("SELECT * FROM freezebe");
+		if (err) {
+			// console.log(err);
+			errorDetected = true;
+			res.status(500).json({ error: "request failed" });
+		}
+		getFreezebes();
 	});
 	connection.connect();
 
-	function executeStatement(req) {
-		data = [];
-		request = new Request(req, function (err) {
+	function getFreezebes() {
+		var result = "";
+		request = new Request("SELECT * FROM freezebe FOR JSON PATH, ROOT('Freezebes')", function (err) {
 			if (err) {
-				console.log(err);
+				// console.log(err);
+				errorDetected = true;
+				res.status(500).json({ error: "Request failed" });
 			}
 		});
-		var result = "";
 		request.on("row", function (columns) {
 			columns.forEach(function (column) {
 				if (column.value === null) {
-					console.log("NULL");
+					// console.log("NULL");
 				} else {
 					result += column.value + " ";
 				}
 			});
 			console.log(result);
-			data.push(result);
-			result = "";
 		});
-
-		request.on("done", function (rowCount, more) {
-			console.log(rowCount + " rows returned");
-		});
-
-		// Close the connection after the final event emitted by the request, after the callback passes
 		request.on("requestCompleted", function (rowCount, more) {
 			connection.close();
-			res.status(200).json(data);
+			if (!errorDetected) {
+				res.status(200).json(result);
+			}
 		});
 		connection.execSql(request);
 	}
 };
 exports.getOne = (req, res, next) => {
-	console.log("GET getOne");
-	res.status(200).json({ msg: "ok" });
+	let errorDetected = false;
+	var connection = new Connection(config);
+	connection.on("connect", function (err) {
+		console.log("CONNEXION ON");
+		if (err) {
+			// console.log(err);
+			errorDetected = true;
+			res.status(500).json({ error: "request failed" });
+		}
+		getFreezebe();
+	});
+	connection.connect();
+
+	function getFreezebe() {
+		var result = "";
+		request = new Request(`SELECT * FROM freezebe WHERE id = @Id FOR JSON PATH, ROOT('Freezebe')`, function (err) {
+			if (err) {
+				// console.log(err);
+				errorDetected = true;
+				res.status(500).json({ error: "Request failed" });
+			}
+		});
+		request.addParameter("Id", TYPES.Int, req.params.id);
+
+		request.on("row", function (columns) {
+			columns.forEach(function (column) {
+				if (column.value === null) {
+					// console.log("NULL");
+				} else {
+					result += column.value + " ";
+				}
+			});
+		});
+		request.on("requestCompleted", function (rowCount, more) {
+			connection.close();
+			if (!errorDetected) {
+				res.status(200).json(result);
+			}
+		});
+		connection.execSql(request);
+	}
 };
 exports.create = (req, res, next) => {
-	console.log("POST create");
-	res.status(200).json({ msg: "ok" });
+	let errorDetected = false;
+	var connection = new Connection(config);
+	connection.on("connect", function (err) {
+		if (err) {
+			// console.log(err);
+			errorDetected = true;
+			res.status(500).json({ error: "request failed" });
+		}
+		insertFreezebe();
+	});
+	connection.connect();
+
+	function insertFreezebe() {
+		request = new Request(
+			"INSERT freezebe (name, description, pUHT, gamme) OUTPUT INSERTED.id VALUES (@Name, @Description, @PUHT, @Gamme);",
+			function (err) {
+				if (err) {
+					// console.log(err);
+					errorDetected = true;
+					res.status(500).json({ error: "Request failed" });
+				}
+			}
+		);
+		request.addParameter("Name", TYPES.VarChar, req.body.name);
+		request.addParameter("Description", TYPES.VarChar, req.body.description);
+		request.addParameter("PUHT", TYPES.Float, req.body.pUHT);
+		request.addParameter("Gamme", TYPES.VarChar, req.body.gamme);
+
+		request.on("requestCompleted", function (rowCount, more) {
+			connection.close();
+			if (!errorDetected) {
+				res.status(200).json({ msg: "Freezebe inserted successfully" });
+			}
+		});
+		connection.execSql(request);
+	}
 };
 exports.update = (req, res, next) => {
-	console.log("POST update");
-	res.status(200).json({ msg: "ok" });
+	let errorDetected = false;
+	var connection = new Connection(config);
+	connection.on("connect", function (err) {
+		if (err) {
+			// console.log(err);
+			errorDetected = true;
+			res.status(500).json({ error: "request failed" });
+		}
+		updateFreezebe();
+	});
+	connection.connect();
+
+	function updateFreezebe() {
+		request = new Request(
+			"UPDATE freezebe SET name = @Name, description = @Description, pUHT = @PUHT, gamme = @Gamme WHERE id = @Id;",
+			function (err) {
+				if (err) {
+					// console.log(err);
+					errorDetected = true;
+					res.status(500).json({ error: "Request failed" });
+				}
+			}
+		);
+		request.addParameter("Id", TYPES.Int, req.params.id);
+		request.addParameter("Name", TYPES.VarChar, req.body.name);
+		request.addParameter("Description", TYPES.VarChar, req.body.description);
+		request.addParameter("PUHT", TYPES.Float, req.body.pUHT);
+		request.addParameter("Gamme", TYPES.VarChar, req.body.gamme);
+
+		request.on("requestCompleted", function (rowCount, more) {
+			connection.close();
+			if (!errorDetected) {
+				res.status(201).json({ msg: "Freezebe updated successfully !" });
+			}
+		});
+		connection.execSql(request);
+	}
 };
 exports.delete = (req, res, next) => {
-	console.log("DELETE delete");
-	res.status(200).json({ msg: "ok" });
+	let errorDetected = false;
+	var connection = new Connection(config);
+	connection.on("connect", function (err) {
+		if (err) {
+			// console.log(err);
+			errorDetected = true;
+			res.status(500).json({ error: "request failed" });
+		}
+		deleteFreezebe();
+	});
+	connection.connect();
+
+	function deleteFreezebe() {
+		request = new Request("DELETE FROM freezebe WHERE id = @Id", function (err) {
+			if (err) {
+				// console.log(err);
+				errorDetected = true;
+				res.status(500).json({ error: "Request failed" });
+			}
+		});
+		request.addParameter("Id", TYPES.Int, req.params.id);
+
+		request.on("requestCompleted", function (rowCount, more) {
+			connection.close();
+			if (!errorDetected) {
+				res.status(200).json({ response: "Freezebe deleted successfully" });
+			}
+		});
+		connection.execSql(request);
+	}
 };
--- routes/freezebe.route.js
+++ routes/freezebe.route.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const freezeb = require('../controllers/freezebe.ctrl');
+const freezeb = require("../controllers/freezebe.ctrl");
 
-router.get('/', freezeb.getAll); //
-router.get('/:id', freezeb.getOne); //
-router.post('/', freezeb.create); //
-router.post('/:id', freezeb.update); //
-router.delete('/:id', freezeb.delete); //
+router.get("/", freezeb.getAll); //ok
+router.get("/:id", freezeb.getOne); //ok
+router.post("/", freezeb.create); //ok
+router.post("/:id", freezeb.update); //ok
+router.delete("/:id", freezeb.delete); //ok
 
 module.exports = router;
