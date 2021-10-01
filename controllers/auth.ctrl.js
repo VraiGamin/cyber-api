39e1a7f6e6510e5d12b7c8809c3ea1c718c438d7
Leo <leo.gabaix@viacesi.fr>
2021-10-01 13:51:36 +0200

    jwt send to client

--- controllers/auth.ctrl.js
+++ controllers/auth.ctrl.js
@@ -1,17 +1,24 @@
 const jwt = require("json-web-token");
 
 exports.checkJWT = (req, res, next) => {
+	// req.accepts("Authorization");
 	try {
-		const token = req.header.authorization.split("")[1];
-		const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+		console.log("Authorization:", req.get("Authorization"));
+		const token = req.get("Authorization").split("")[1];
+		console.log("token1");
+		const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+		console.log("token");
+		console.log("token:", decodedToken);
 		const userEmail = decodedToken.userEmail;
 		if (req.body.userEmail && req.body.userEmail !== userEmail) {
 			throw "User ID non valable";
 		} else {
+			console.log("success");
 			next();
 		}
 	} catch (error) {
 		res.status(401).json({ error: error || "requete non authentifiée" });
+		// console.log("Authorization:", req.authorization);
 	}
 };
 
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/users.ctrl");
+const authChecker = require("../controllers/auth.ctrl");
 
 router.post("/login", userCtrl.login); //
-router.get("/", userCtrl.getAll); //
+router.get("/", authChecker.checkJWT, userCtrl.getAll); //
 router.get("/:id", userCtrl.getOne); //
 router.post("/", userCtrl.create); //
 router.put("/:id", userCtrl.update); //
